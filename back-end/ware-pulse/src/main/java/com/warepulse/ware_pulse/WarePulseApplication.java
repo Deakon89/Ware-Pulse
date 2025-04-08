8a1f8294f60211c12f0176947e118cabdcf580a1
Deakon89 <franck.devw@gmail.com>
2025-04-08 12:47:33 +0200

    upd-1 frist-step
    creazione progetto e inizializzazione angular e java

---
+++ back-end/ware-pulse/src/main/java/com/warepulse/ware_pulse/WarePulseApplication.java
@@ -0,0 +1,13 @@
+package com.warepulse.ware_pulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarePulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarePulseApplication.class, args);
+	}
+
+}
