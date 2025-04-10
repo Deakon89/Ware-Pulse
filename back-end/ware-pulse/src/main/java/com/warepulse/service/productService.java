57f04808e6e34a5254cf5066c9a03da21240220b
Deakon89 <franck.devw@gmail.com>
2025-04-10 14:17:20 +0200

    update

--- back-end/ware-pulse/src/main/java/com/warepulse/service/productService.java
+++ back-end/ware-pulse/src/main/java/com/warepulse/service/productService.java
@@ -1,34 +1,37 @@
 package com.warepulse.service;
 
-// import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Service;
 import com.warepulse.model.Product;
+import com.warepulse.model.product;
 import com.warepulse.repository.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class ProductService {
 
     @Autowired
     private ProductRepo productRepo;
 
-    // all product
-    public List<Product> findAll() {
-        return productRepo.findAll();
-    }
-    // product by id
-    public Optional<Product> getProductById(Long id) {
-        Optional<Product> product = productRepo.findById(id);
-        return product;
-    }
-    // save a product
-    public Product saveProduct(Product product) {
-        return productRepo.save(product);
+    // // Get all products
+     public List<Product> getProducts() {
+         return productRepo.findAll();
+     }
+    // // product by id
+     public Optional<Product> getProductById(Long id) {
+         Optional<Product> product = productRepo.findById(id);
+         return product;
+     }
+    // // save a product
+     public Product saveProduct(Product product) {
+         return productRepo.save(product);
     }
     // delete a product
-    public void deleteProduct(Long id) {
+    public void deleteProduct(Long id){
         productRepo.deleteById(id);
     }
     
+
 }
--- back-end/ware-pulse/src/main/java/com/warepulse/ware_pulse/WarePulseApplication.java
+++ back-end/ware-pulse/src/main/java/com/warepulse/ware_pulse/WarePulseApplication.java
@@ -2,8 +2,12 @@ package com.warepulse.ware_pulse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.warepulse"})
+@EntityScan(basePackages = {"com.warepulse.model"})
+@EnableJpaRepositories(basePackages = {"com.warepulse.repository"})
 public class WarePulseApplication {
 
 	public static void main(String[] args) {
