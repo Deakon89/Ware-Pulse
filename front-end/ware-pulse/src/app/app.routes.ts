51a5b99aecb96f3cdd0c7bd4f8d19dfd9df8b1bf
Deakon89 <franck.devw@gmail.com>
2025-04-23 11:15:33 +0200

    add component

--- front-end/ware-pulse/src/app/app.routes.ts
+++ front-end/ware-pulse/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { AboutComponent } from './component/about/about.component';
+import { LoginComponent } from './component/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent, children: [
+        { path: 'about', component: AboutComponent },
+        { path: 'login', component: LoginComponent },
+    ] },
+];
--- front-end/ware-pulse/src/app/component/home/home.component.ts
+++ front-end/ware-pulse/src/app/component/home/home.component.ts
@@ -1,11 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
+// import { routeAnimations } from '../../route-animation';
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule, RouterOutlet, CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+  // animations: [routeAnimations],
+  template: `
+  //   <div [@routeAnimations]="prepareRoute(outlet)">
+  //     <router-outlet #outlet="outlet"></router-outlet>
+  //   </div>
+  // `
 })
+// main page
 export class HomeComponent {
+ 
+  menuActive: boolean = false;
+  prepareRoute(outlet: RouterOutlet) {
+    return outlet?.activatedRouteData?.['animation'];
+  }
 
+  toggleMenu() {
+    this.menuActive = !this.menuActive;
+  }
+
+  closeMenu() {
+    this.menuActive = false;
+  }
+
+
+  hasChildRoute(outlet: RouterOutlet): boolean {
+    return outlet.isActivated;
+  }
 }
