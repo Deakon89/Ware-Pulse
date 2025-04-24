58e0f3850bc626d020b8c8a7d49b336747d50018
Deakon89 <franck.devw@gmail.com>
2025-04-24 12:26:50 +0200

    ad dash

--- front-end/ware-pulse/src/app/component/dashboard/dashboard.component.ts
+++ front-end/ware-pulse/src/app/component/dashboard/dashboard.component.ts
@@ -5,6 +5,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 
+
+
 @Component({
   selector: 'app-sidebar',
   standalone: true,
@@ -18,5 +20,7 @@ import { MatListModule } from '@angular/material/list';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+
+}
 
