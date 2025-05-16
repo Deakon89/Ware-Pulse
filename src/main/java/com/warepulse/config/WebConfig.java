42a44c29fc0e63022271c726b494206ecfdbebfe
Deakon89 <franck.devw@gmail.com>
2025-05-16 22:42:38 +0200

    Update WebConfig.java

--- src/main/java/com/warepulse/config/WebConfig.java
+++ src/main/java/com/warepulse/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
   @SuppressWarnings("null")
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/api/**")
+    registry.addMapping("/**")
             .allowedOrigins("https://ware-pulse.netlify.app")
             .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
             .allowedHeaders("*")
