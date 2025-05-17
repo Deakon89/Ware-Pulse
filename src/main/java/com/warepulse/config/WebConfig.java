5bbe57eaf3cd39726d87e28dec93c6aae34a9238
Deakon89 <franck.devw@gmail.com>
2025-05-17 13:07:26 +0200

    Update WebConfig.java

--- src/main/java/com/warepulse/config/WebConfig.java
+++ src/main/java/com/warepulse/config/WebConfig.java
@@ -2,20 +2,20 @@
 //package com.warepulse.config;
 
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.*;
+//import org.springframework.context.annotation.Configuration;
+//import //org.springframework.web.servlet.config.annotation.*;
 
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
+//@Configuration
+//public class WebConfig implements //WebMvcConfigurer {
 
-  @SuppressWarnings("null")
-  @Override
-  public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**")
-            .allowedOrigins("https://ware-pulse.netlify.app")
-            .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
-            .allowedHeaders("*")
-            .allowCredentials(true);
-  }
-}//
+//  @SuppressWarnings("null")
+//  @Override
+ // public void addCorsMappings(CorsRegistry registry) {
+    //registry.addMapping("/**")
+          //  .allowedOrigins("https://ware-pulse.netlify.app")
+           // .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+           // .allowedHeaders("*")
+           // .allowCredentials(true);
+ // }
+//}
 
